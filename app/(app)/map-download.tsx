238be624d64f54fc693ed46161d128aab0a0c206
pinokokol <pino.kokol@gmail.com>
2024-03-19 19:18:39 +0100

    offline map download

--- app/(app)/map-download.tsx
+++ app/(app)/map-download.tsx
@@ -1,8 +1,8 @@
 import OfflinePack from '@rnmapbox/maps/lib/typescript/src/modules/offline/OfflinePack';
 import { useNavigation } from 'expo-router';
-import { ChevronLeft } from 'lucide-react-native';
+import { ChevronLeft, Download } from 'lucide-react-native';
 import { createRef, useEffect, useState } from 'react';
-import { Alert, Pressable, Text, View } from 'react-native';
+import { Alert, Pressable, ScrollView, Text, View } from 'react-native';
 import Mapbox from '@rnmapbox/maps';
 import i18n from '@/locales/i18n';
 import * as Location from 'expo-location';
@@ -16,6 +16,7 @@ export default function MapDownload() {
   const [offlinePacks, setOfflinePacks] = useState<OfflinePack[]>([]);
   const [bounds, setBounds] = useState<any | null>(null);
   const [mapLoaded, setMapLoaded] = useState<boolean>(false);
+  const [selectingMap, setSelectingMap] = useState<boolean>(false);
 
   const mapRef = createRef<Mapbox.MapView>();
 
@@ -76,59 +77,121 @@ export default function MapDownload() {
     }
   };
 
-  const onDownloadArea = () => {
-    if (bounds) {
-      // Define offline pack bounds using the current visible bounds
-      const offlinePackBounds = [
-        [bounds[0][0], bounds[0][1]],
-        [bounds[1][0], bounds[1][1]],
-      ];
-
-      // Proceed with Mapbox offline pack download using offlinePackBounds...
+  const onDownloadArea = async () => {
+    if (!bounds) {
+      Alert.alert('No area selected', 'Please select an area to download.');
+      return;
     }
-    Alert.alert('Download area not implemented');
+
+    const metadata = { name: 'Your Map Name', date: new Date().toISOString() };
+
+    try {
+      await Mapbox.offlineManager.createPack(
+        {
+          name: 'Your Map Name',
+          styleURL: Mapbox.StyleURL.Street,
+          bounds: [
+            [bounds[0][1], bounds[0][0]],
+            [bounds[1][1], bounds[1][0]],
+          ],
+          minZoom: 14,
+          maxZoom: 20,
+          metadata,
+        },
+        (pack) => {}
+      );
+
+      Alert.alert('Download Started', 'Your map is now downloading.');
+    } catch (error) {
+      console.error('Error starting offline pack download:', error);
+      Alert.alert(
+        'Download Error',
+        'There was an error starting the download.'
+      );
+    }
   };
 
   return (
     <View className="h-full">
-      {offlinePacks.length > 0 ? (
-        <>
-          <Text>Downloaded Maps:</Text>
-          {offlinePacks.map((pack, index) => (
-            <Text key={index}>{pack.name}:</Text>
-          ))}
-          <Text>Total Size: 0</Text>
-        </>
-      ) : (
-        <Text>No offline maps downloaded.</Text>
-      )}
-      <View className="flex-1 h-full">
-        {location && (
-          <Mapbox.MapView
-            className="flex-1"
-            ref={mapRef}
-            onMapIdle={onRegionDidChange}
-            rotateEnabled={false}
-            pitchEnabled={false}
-          >
-            <Mapbox.Camera
-              zoomLevel={14}
-              centerCoordinate={[
-                location.coords.longitude,
-                location.coords.latitude,
-              ]}
-            />
-            {mapLoaded && location && (
-              <Mapbox.UserLocation
-                onUpdate={(newLocation) => {}} //setLocation(newLocation)}
+      {selectingMap ? (
+        <View className="flex-1 h-full">
+          <View className="flex-row items-center justify-center w-full p-5 bg-White flew">
+            <Text className="text-[20px]">
+              {i18n.t('plots.offlineMapsScreen.downloadThisMap')}
+            </Text>
+          </View>
+          {location && (
+            <Mapbox.MapView
+              className="flex-1"
+              ref={mapRef}
+              onMapIdle={onRegionDidChange}
+              rotateEnabled={false}
+              pitchEnabled={false}
+            >
+              <Mapbox.Camera
+                zoomLevel={14}
+                centerCoordinate={[
+                  location.coords.longitude,
+                  location.coords.latitude,
+                ]}
               />
-            )}
-          </Mapbox.MapView>
-        )}
-        <Pressable onPress={onDownloadArea}>
-          <Text>Download Selected Area</Text>
-        </Pressable>
-      </View>
+              {mapLoaded && location && (
+                <Mapbox.UserLocation
+                  onUpdate={(newLocation) => {}} //setLocation(newLocation)}
+                />
+              )}
+            </Mapbox.MapView>
+          )}
+
+          <View className="flex flex-row items-center justify-between p-5 bg-White">
+            <Pressable
+              onPress={() => setSelectingMap(false)}
+              className="w-[55%] px-5 py-3 rounded-md bg-Green flex flex-row items-center justify-center"
+            >
+              <Text className="text-White font-semibold text-[16px]">
+                {i18n.t('plots.offlineMapsScreen.cancel')}
+              </Text>
+            </Pressable>
+            <Pressable
+              onPress={onDownloadArea}
+              className="flex flex-row items-center justify-center w-[40%] px-5 py-3 rounded-md bg-Orange"
+            >
+              <Download className="mr-2 text-White" size={20} />
+              <Text className="text-White font-semibold text-[16px]">
+                {i18n.t('plots.offlineMapsScreen.download')}
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      ) : (
+        <View className="m-5">
+          <Pressable
+            onPress={() => setSelectingMap(true)}
+            className="flex flex-row items-center justify-center h-12 px-2 rounded-md bg-Orange"
+          >
+            <Download className="mr-2 text-White" size={20} />
+            <Text className="font-semibold text-[16px] text-White">
+              {i18n.t('plots.offlineMapsScreen.selectYourOwnMap')}
+            </Text>
+          </Pressable>
+          {offlinePacks.length > 0 ? (
+            <ScrollView>
+              <Text>Downloaded Maps:</Text>
+              {offlinePacks.map((pack, index) => (
+                <Text key={index}>{pack.name}:</Text>
+              ))}
+              <Text>Total Size: 0</Text>
+            </ScrollView>
+          ) : (
+            <View className="mt-5">
+              <Text className="text-[18px] font-medium">
+                {i18n.t('plots.offlineMapsScreen.downloadedMaps')}
+              </Text>
+              <Text>{i18n.t('plots.offlineMapsScreen.noMapsDownloaded')}</Text>
+            </View>
+          )}
+        </View>
+      )}
     </View>
   );
 }
